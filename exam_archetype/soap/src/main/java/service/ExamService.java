4c65a625e9f171301070cf6e57f80ffa5e2993a3
KonradPolarczyk <konradpolarczyk98@gmail.com>
2020-07-12 14:21:02 +0200

    exam working program

--- exam_archetype/soap/src/main/java/service/ExamService.java
+++ exam_archetype/soap/src/main/java/service/ExamService.java
@@ -1,5 +1,8 @@
 package service;
 
+import javax.annotation.security.DeclareRoles;
+import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.jws.WebMethod;
@@ -8,9 +11,14 @@ import javax.jws.WebService;
 import db.DatabaseManager;
 import entity.Message;
 import jms.QueueSender;
+import org.jboss.annotation.security.SecurityDomain;
+import org.jboss.ws.api.annotation.WebContext;
 
+@SecurityDomain("exam-security-domain")
+@DeclareRoles("exam_group")
 @Stateless
 @WebService(name = "exam_service")
+@WebContext(authMethod = "BASIC")
 public class ExamService {
 
     @Inject
@@ -19,6 +27,7 @@ public class ExamService {
     @Inject
     QueueSender queueSender;
 
+    @PermitAll
     @WebMethod
     public long push(String data) {
         long id = dbManager.saveData(data);
@@ -26,6 +35,7 @@ public class ExamService {
         return id;
     }
 
+    @RolesAllowed("exam_group")
     @WebMethod
     public boolean check(long id) {
         Message msg = dbManager.getMessage(id);
